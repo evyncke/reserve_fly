4ecbbcc3d0cabd0b70f75928f8b906474926efa9
Eric Vyncke <eric@vyncke.org>
2020-03-20 12:01:35 +0100

    Add a button for maintenance calendar

--- reservation.php
+++ reservation.php
@@ -373,6 +373,7 @@ if ($userId == 0) {
 	print('<input type="button" value="Carte de mes vols" onclick="javascript:document.location.href=\'mymap.php\';"> ') ;
 	print('<input type="button" value="Mobile" onclick="javascript:document.location.href=\'mobile.php?news\';"> ') ;
 	if ($userIsAdmin) print('<input type="button" value="Journal des opérations" onclick="javascript:document.location.href=\'journal.php\';"> ') ;
+	if ($userIsAdmin || $userIsMechanic) print('<input type="button" value="Echéances des maintenances" onclick="javascript:document.location.href=\'plane_planning.php\';"> ') ;
 	print('<input type="button" value="No log" style="background-color: yellow; visibility: hidden;" id="logButton" onclick="javascript:toggleLogDisplay();"> ') ;
 	// TODO check whether webcall works with Clouflare
 	print("<a href=\"webcal://$_SERVER[SERVER_NAME]/resa/ics.php?user=$userId&auth=" . md5($userId . $shared_secret) . "\">lier &agrave; mon calendrier (iCal)</a>") ;
