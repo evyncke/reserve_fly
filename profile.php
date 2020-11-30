a8e12b1b01b8f0841af4fab2ce0cd1cb802f7dda
Eric Vyncke <eric@vyncke.org>
2020-11-30 09:20:11 +0100

    Add MATOMO analytics

--- profile.php
+++ profile.php
@@ -261,6 +261,23 @@ const
 <!--- cacheable data -->
 <script data-cfasync="true" src="profile.js"></script>
 <script data-cfasync="true" src="members.js"></script>
+<!-- Matomo -->
+<script type="text/javascript">
+  var _paq = window._paq = window._paq || [];
+  /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+  _paq.push(["setDocumentTitle", document.domain + "/" + document.title]);
+  _paq.push(["setCookieDomain", "*.spa-aviation.be"]);
+  _paq.push(['trackPageView']);
+  _paq.push(['enableLinkTracking']);
+  (function() {
+    var u="//analytics.vyncke.org/";
+    _paq.push(['setTrackerUrl', u+'matomo.php']);
+    _paq.push(['setSiteId', '5']);
+    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+    g.type='text/javascript'; g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
+  })();
+</script>
+<!-- End Matomo Code -->
 </head>
 <body onload="init(<?=$displayed_id?>);">
 
