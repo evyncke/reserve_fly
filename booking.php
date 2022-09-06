b5e0ff59d0b333e30d7e6fea6bb2053f9376956c
Eric Vyncke <eric@vyncke.org>
2022-09-06 11:03:51 +0200

    Handle the case for joint booking and next/prvious

--- booking.php
+++ booking.php
@@ -1,6 +1,6 @@
 <?php
 /*
-   Copyright 2013-2020 Eric Vyncke
+   Copyright 2013-2022 Eric Vyncke
 
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
@@ -72,13 +72,13 @@ else {
 
 // Find the previous/next booking
 $result = mysqli_query($mysqli_link, "select * from $table_bookings JOIN $table_planes on r_plane = $table_planes.id
-		where ressource = 0 and r_cancel_date is null and r_stop < '$booking[r_start]' and $condition order by r_start desc limit 0,1")
+		where ressource = 0 and r_cancel_date is null and r_stop <= '$booking[r_start]' and $condition order by r_start desc limit 0,1")
 	or die("Cannot access previous booking: ".mysqli_error()) ;
 $row = mysqli_fetch_array($result) ;
 $previous_id = $row['r_id'] ;
 $previous_auth = md5($previous_id . $shared_secret) ;
 $result = mysqli_query($mysqli_link, "select * from $table_bookings JOIN $table_planes ON r_plane = $table_planes.id
-		where ressource = 0 and r_cancel_date is null and r_start > '$booking[r_stop]' and $condition order by r_start asc limit 0,1")
+		where ressource = 0 and r_cancel_date is null and r_start >= '$booking[r_stop]' and $condition order by r_start asc limit 0,1")
 	or die("Cannot access previous booking: ".mysqli_error()) ;
 $row = mysqli_fetch_array($result) ;
 $next_id = $row['r_id'] ;
