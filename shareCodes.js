3f81c4dc700058084861b977a6283a79bd2ccc08
Eric Vyncke <eric@vyncke.org>
2022-11-01 07:42:48 +0100

    Add mechanics code

--- shareCodes.js
+++ shareCodes.js
@@ -1,8 +1,10 @@
 var shareCodes = [ { id: 0, name: "Aucun partage"},
-	{ id: -1, name: "Ferry Aéroclub"},
-	{ id: -2, name: "Aéroclub"},
+	{ id: -1, name: "Ferry Club"},
+	{ id: -2, name: "Club"},
 	{ id: -3, name: "Initiation"},
 	{ id: -4, name: "Vol IF"},
 	{ id: -5, name: "Vol membre"},
-	{ id: -6, name: "Vol Domaine Hautes Fagnes"}
+        { id: -6, name: "Vol D.H.F."},
+        { id: -7, name: "Vol Président"},
+        { id: -8, name: "Mécano"}
 ]; 
