e6c3947dbcd16a287b172fea03912eb09feb025e
Eric Vyncke <eric@vyncke.org>
2023-01-02 08:58:50 +0100

    Allow UTF-8 in block reason

--- reservation.php
+++ reservation.php
@@ -382,7 +382,16 @@ if ($userNoFlight) print(" <span style=\"color: red;\">interdit de vol</span> ")
 if (! ($userIsPilot || $userIsAdmin || $userIsInstructor || $userIsMechanic))
 	print("<br/><font color=red>Vous devez &ecirc;tre au moins pilote pour r&eacute;server un avion.</font>") ;
 if ($userNoFlight)
-	print("<div class=\"noFlyBox\">Vous &ecirc;tes interdit(e) de vol (par exemple: factures non pay&eacute;es, contactez <a href=\"mailto:info@spa-aviation.be\">l'a&eacute;roclub).</div>") ;
+	print("<div class=\"noFlyBox\">Vous &ecirc;tes interdit(e) de vol (par exemple: factures non pay&eacute;es, contactez <a href=\"mailto:info@spa-aviation.be\">info@spa-aviation.be.</div>") ;
+// Check whether the user is blocked
+$result_blocked = mysqli_query($mysqli_link, "SELECT * FROM $table_blocked WHERE b_jom_id=$userId")
+	or journalise($userId, 'E', "Cannot checked whether user is blocked: " . mysqli_error($mysqli_link)) ;
+$row_blocked = mysqli_fetch_array($result_blocked) ;
+if ($row_blocked) {
+	journalise($userId, "W", "This user is blocked: " . db2web($row_blocked['b_reason'])) ;
+	$userNoFlight = true ;
+	print("<div class=\"noFlyBox\">Vous &ecirc;tes interdit(e) de vol: <b>" . db2web($row_blocked['b_reason']) . "</b>. Contactez <a href=\"mailto:info@spa-aviation.be\">l'a&eacute;roclub).</div>") ;
+}
 if ($userId == 0) {
 	print("<br/><font color=red>Vous devez &ecirc;tre connect&eacute;(e) pour r&eacute;server un avion.</font> ") ;
 } else {
@@ -554,7 +563,7 @@ $result_customer = mysqli_query($mysqli_link, "SELECT *
 	ORDER BY f_date_assigned")
 	or die("Cannot retrieve customers: " . mysqli_error($mysqli_link)) ;
 if (mysqli_num_rows($result_customer)) {
-	print("Client: <select id=\"customerSelect\">
+	print("Client vol découverte/initiation: <select id=\"customerSelect\">
 		<option value=\"-1\"> - aucun - </option>\n") ;
 	while ($row_customer = mysqli_fetch_array($result_customer)) {
 			print("<option value=\"$row_customer[f_id]\">" . db2web("$row_customer[p_fname] $row_customer[p_lname] (IF)") . "</option>\n") ;
