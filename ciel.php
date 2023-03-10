836bff04d43441f79e7a392ef35ba92b2a363936
Eric Vyncke <eric@vyncke.org>
2023-03-10 17:22:48 +0100

    List members w/o book keeping account

--- ciel.php
+++ ciel.php
@@ -53,7 +53,7 @@ if (! $userIsAdmin && ! $userIsBoardMember)
 
 <a href="#" class="btn btn-primary" role="button">Import des factures</a>
 
-<h2>Import du grand livre client Ciel dans le site</h2>
+<h2>Import du grand livre clients Ciel dans le site</h2>
 <p>Cette opération permet d'importer les mouvements comptables (factures, notes de crédit, paiements, ...) et les soldes de Ciel dans les "comptes membres". 
     Elle peut se faire autant de fois que souhaité (les mouvements précédents sont alors remplacés par les nouveaux sur le site).</p>
     <p>Avant de lancer cet import, il est nécesssaire d'avoir exporté le grand livre client depuis <i>Ciel Account Premium</i>:<ol>
@@ -69,5 +69,27 @@ if (! $userIsAdmin && ! $userIsBoardMember)
   Grand livre à importer dans le site: <input type="file" name="ledgerFile" id="ledgerFile">
     <input type="submit" value="Import" class="btn btn-primary" name="submit">
 </form>
+
+<h2>Membres inexistants dans Ciel</h2>
+<p>Liste des membres du club dont le numéro de compte Ciel est inconnu.</p>
+<table class="table table-striped table-hover table-responsive">
+    <thead>
+        <tr><th>Nom utilisateur</th><th>Nom</th><th>Prénom</th><th>email</th><th>Date création</th></tr>
+    </thead>
+    <tbody>
+<?php
+$result = mysqli_query($mysqli_link, "SELECT * FROM $table_person JOIN jom_users AS j ON jom_id = j.id
+    WHERE ciel_code400 IS NULL
+    ORDER BY j.registerDate DESC")
+    or journalise($userId, "F", "Cannot read members: " . mysqli_error($mysqli_link)) ;
+while ($row = mysqli_fetch_array($result)) {
+    $name = db2web($row['name']) ;
+    $first_name = db2web($row['first_name']) ;
+    $last_name = db2web($row['last_name']) ;
+    print("<tr><td>$row[username]</td><td>$last_name</td><td>$first_name</td><td>$row[email]</td><td>$row[registerDate]</td></tr>\n") ;
+}
+?>
+</tbody>
+</table>
 </body>
 </html>
