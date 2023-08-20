c610be84b30edf3832a2cedcda8bc641702a0d5a
Eric Vyncke <eric@vyncke.org>
2023-08-20 09:17:56 +0200

    Add journal and admin in the menu

---
+++ mobile_journal.php
@@ -0,0 +1,88 @@
+<?php
+/*
+   Copyright 2014-2023 Eric Vyncke
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+
+*/
+
+ob_start("ob_gzhandler");
+require_once "dbi.php" ;
+require_once 'mobile_header5.php' ;
+
+if (!$userIsAdmin) journalise($userId, "F", "This admin page is reserved to administrators") ;
+
+?><div class="container-fluid">
+<h2>Journal système</h2>
+
+<div class="table-responsive">
+<table class="table table-striped table-hover">
+<thead>
+<tr>
+<th>Date</th>
+<th>User</th>
+<th class="d-none d-lg-block">IP Address</th>
+<th>Message</th>
+<th class="d-none d-lg-block">URI</th>
+</tr>
+</thead>
+<tbody>
+<?php
+$start = (isset($_REQUEST['start'])) ? $_REQUEST['start'] : 99999999 ;
+$sql_filter = '' ;
+if (isset($_REQUEST['id']) and is_numeric($_REQUEST['id']))
+	$sql_filter = " AND j_jom_id = $_REQUEST[id]" ;
+  else if (isset($_REQUEST['user']))
+	$sql_filter = " AND name like '%" . web2db($_REQUEST['user']) . "%'" ;
+ else if (isset($_REQUEST['msg']))
+	$sql_filter = " AND j_message like '%" . web2db($_REQUEST['msg']) . "%'" ;
+$sql = "SELECT * FROM $table_journal LEFT JOIN $table_users u ON j_jom_id = u.id
+		WHERE j_id <= $start $sql_filter
+		ORDER BY j_id desc
+		LIMIT 0, 50" ;
+$result = mysqli_query($mysqli_link, $sql) or die("Erreur systeme a propos de l'access au journal: " . mysqli_error($mysqli_link)) ;
+$first_id = -1 ;
+while ($row = mysqli_fetch_array($result)) {
+	if ($first_id < 0) $first_id = $row['j_id'] ;
+	$line_count ++ ;
+	$last_id = $row['j_id'] ;
+	$nameStyle = ($row['j_trusted_booker'] == 1) ? ' style="font-weight: bold;"' : '' ;
+	switch (strtoupper($row['j_severity'])) {
+		case 'F': $rowStyle = ' class="bg-danger"' ; break ;
+		case 'E': $rowStyle = ' class="bg-warning"' ; break ;
+		case 'W': $rowStyle = ' class="text-info"' ; break ;
+		default: $rowStyle = '' ;
+	}
+	print("<tr>
+		<td$rowStyle>$row[j_datetime]</td>
+		<td$nameStyle$rowStyle>" . db2web($row['name']) . "</td>
+		<td$rowStyle class=\"d-none d-lg-table-cell\">$row[j_address]</td>
+		<td$rowStyle class=\"text-align\">" . db2web($row['j_message']) . "</td>
+		<td$rowStyle class=\"text-align d-none d-lg-table-cell\">" . db2web($row['j_uri']) . "</td>
+		</tr>\n") ;
+}
+$first_id += 50 ;
+$last_id -= 1 ;
+?>
+</tbody>
+</table>
+<ul class="pagination justify-content-center">
+  <li  class="page-item"><a class="page-link" href="<?="$_SERVER[PHP_SELF]?start=$last_id"?>"><i class="bi bi-caret-left-fill"></i> Entrées précédentes</a></li>
+  <li  class="page-item"><a class="page-link" href="<?="$_SERVER[PHP_SELF]?start=$first_id"?>"><i class="bi bi-caret-right-fill"></i> Entrées suivantes</a></li>
+  <li  class="page-item"><a class="page-link" href="<?="$_SERVER[PHP_SELF]"?>"><i class="bi bi-caret-right-fill"></i><i class="bi bi-caret-right-fill"></i> Dernières entrées</a></li>
+</ul><!-- pagination -->
+</div> <!-- table responsive -->
+Les heures sont les heures locales.<br/>
+</div><!-- container -->
+</body>
+</html>
