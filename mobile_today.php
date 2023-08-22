2a1af1be54d9b8005a1a85ef8cdde886c8272af5
Eric Vyncke <eric@vyncke.org>
2023-08-22 08:32:07 +0200

    Different color for IF/INIT flights

--- mobile_today.php
+++ mobile_today.php
@@ -52,10 +52,11 @@ if ($userId > 0) { // Only members can see all bookings
 <?php
 	$result = mysqli_query($mysqli_link, "SELECT *, i.last_name as ilast_name, i.first_name as ifirst_name, i.cell_phone as icell_phone, i.jom_id as iid,
 		pi.last_name as plast_name, pi.first_name as pfirst_name, pi.name as pname, pi.cell_phone as pcell_phone, pi.jom_id as pid
-		FROM $table_bookings 
+		FROM $table_bookings b
 		JOIN $table_person pi ON pi.jom_id = r_pilot
 		LEFT JOIN $table_person i ON i.jom_id = r_instructor		
 		JOIN $table_planes p ON r_plane = p.id
+		LEFT JOIN $table_flights fl ON r_id = f_booking
 		WHERE  p.actif = 1 AND p.ressource = 0 AND r_cancel_date IS NULL AND (DATE(r_stop) = '$sql_date' OR (DATE(r_start) <= '$sql_date' and '$sql_date' <= DATE(r_stop)))
 		ORDER BY r_start, r_plane ASC LIMIT 0,20")
 		or die("Cannot retrieve bookings($plane): " . mysqli_error($mysqli_link)) ;
@@ -68,6 +69,8 @@ if ($userId > 0) { // Only members can see all bookings
 			db2web($row['ifirst_name']) . ' ' . db2web($row['ilast_name']) . '">' .
 			substr($row['ifirst_name'], 0, 1) . "." . substr($row['ilast_name'], 0, 1) . '. </span></i>' . $itelephone : '' ; 
 		$class = ($row['r_type'] == BOOKING_MAINTENANCE) ? ' class="text-danger"' : '' ;
+		if ($row['f_type'] != '')
+			$class = ' class="text-warning"' ;
 		// Display date if not today, else display time
 		if (strpos($row['r_start'], $sql_date) === 0) 
 			$row['r_start'] = substr($row['r_start'], 11) ;
