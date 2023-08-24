692f3a51f843f31c1efe141e983985f8532aeb1c
Eric Vyncke <eric@vyncke.org>
2023-08-24 18:20:02 +0200

    Use bootstrap5 style

--- payconiq/pay.php
+++ payconiq/pay.php
@@ -10,10 +10,16 @@ $description = $_REQUEST['description'] ;
 $cb = $_REQUEST['cb'] ;
 
 $pcnq->receivePayment($amount, $description, $reference, 'https://www.spa-aviation.be/resa/payconiq/callback.php') ;
-?>
+?><!DOCTYPE html>
 <html>
 <header>
 <title>Payer avec Payconiq</title>
+<meta http-equiv="content-type" content="text/html; charset=utf-8"/>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link href="<?=$favicon?>" rel="shortcut icon" type="image/vnd.microsoft.icon" />
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"></script>
 <script>
 var paymentId = "<?=$pcnq->paymentId?>" ;
 statusSpan = false ;
@@ -33,6 +39,7 @@ function refreshStatus() {
 				statusSpan.innerHTML = status_response.status ;
 				if(status_response.status != 'EXPIRED' && status_response.status != 'SUCCEEDED') {
 					setTimeout(refreshStatus, 2 * 1000) ;
+                                        statusSpan.innerHTML = '<div class="spinner-border text-black"></div>' + statusSpan.innerHTML ;
 					// TODO display a right message and possible redirect to the referer
 				}
 			} // XHR.status  == 200
@@ -55,12 +62,14 @@ function init() {
 Payment amount: <?=$amount?> &euro;<br/>
 Payment description: <?=$description?><br/>
 Payment reference: <?=$reference?><br/>
-QR code URI: <?=$pcnq->QRCodeURI?><br/>
 paymentId: <?=$pcnq->paymentId?><br/>
 expiresAt: <?=$pcnq->response['expiresAt']?><br/>
 
+Cancel URI: <a href="<?=$pcnq->cancelURI?>"><?=$pcnq->cancelURI?></a><br/>
+QR code URI: <?=$pcnq->QRCodeURI?><br/>
+
 <img src="<?=$pcnq->QRCodeURI?>">
 
-<span id="statusSpan">Waiting for status</span>
+<div class="bg-info"><span id="statusSpan"><div class="spinner-border text-info"></div> Waiting for status</span></div>
 </body>
 </html>
