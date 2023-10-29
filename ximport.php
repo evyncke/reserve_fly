ffe815bf42f6c12b6678cbaed6806efc8bec925e
Patrick Reginster <patrick.reginster@gmail.com>
2023-10-29 18:22:30 +0100

    Test de push

--- ximport.php
+++ ximport.php
@@ -13,9 +13,7 @@
    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
    See the License for the specific language governing permissions and
    limitations under the License.
-
 */
-
 $production = true ; // when production is set to true, invoices are inserted in $table_invoices and are shown to end-users
 
 require_once 'dbi.php' ;
