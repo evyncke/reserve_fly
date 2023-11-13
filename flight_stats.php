d4214880666bf29269b6ee1716764acbc13d31d5
Eric Vyncke <eric@vyncke.org>
2023-11-13 08:13:58 +0100

    Also display all flights

--- flight_stats.php
+++ flight_stats.php
@@ -66,10 +66,13 @@ $monthName = datefmt_format($fmt, $sinceDate) ;
 <tr><th></th><th>Vols découvertes (IF)</th><th>Vols d'initiations (INIT)</th></tr>
 </thead>
 <?php
-$result = mysqli_query($mysqli_link, "SELECT *, 60 * (l_end_hour - l_start_hour) + l_end_minute - l_start_minute as duration,
-		SUM(fl_amount) as revenue
-	FROM $table_flight
+$result = mysqli_query($mysqli_link, "SELECT *, 
+		60 * (l_end_hour - l_start_hour) + l_end_minute - l_start_minute as duration,
+		DATE(l_start) AS date,
+		SUM(fl_amount) AS revenue
+	FROM $table_flight AS f
 		JOIN $table_logbook AS l ON f_booking = l.l_booking
+		JOIN $table_person ON f.f_pilot = jom_id
 		LEFT JOIN $table_flights_ledger ON fl_flight = f_id
 	WHERE '$since' <= l_start AND l_start < '$monthAfterString'
 	GROUP BY f_id
@@ -78,15 +81,22 @@ $result = mysqli_query($mysqli_link, "SELECT *, 60 * (l_end_hour - l_start_hour)
 $count_if = 0 ; $count_init = 0 ;
 $minutes_if = 0 ; $minutes_init = 0 ;
 $revenue_if = 0 ; $revenue_init = 0 ;
+$all_inits = [] ;
+$all_ifs = [] ;
 while ($row = mysqli_fetch_array($result)) {
+	$tr = "<tr><td>$row[date]</td><td>$row[f_reference]</td><td>$row[l_plane]</td><td>" . 
+		db2web("<b>$row[last_name]</b> $row[first_name]") .
+		"</td><td>$row[revenue] &euro;</td><td>$row[duration] min</td></tr>\n" ;
 	switch ($row['f_type']) {
 		case 'D': $count_if++ ;
 			$minutes_if += $row['duration'] ;
 			$revenue_if += $row['revenue'] ;
+			$all_ifs[] = $tr ;
 			break ;
 		case 'I': $count_init++ ;
 			$minutes_init += $row['duration'] ;
 			$revenue_init += $row['revenue'] ;
+			$all_inits[] = $tr ;
 			break ;
 		default:
 			journalise($userId, "E", "Flight $row[f_id] has an invalid type $row[f_type]") ;
@@ -98,6 +108,30 @@ while ($row = mysqli_fetch_array($result)) {
 <tr><td>Minutes</td><td><?=$minutes_if?></td><td><?=$minutes_init?></td></tr>
 <tr><td>Chiffre d'affaires</td><td><?=$revenue_if?> &euro;</td><td><?=$revenue_init?> &euro;</td></tr>
 </tbody>
+</table>
+<h2>Vols d'initiations (INIT)</h2>
+<table class="table table-striped table-responsive col-md-6 col-xs-12">
+	<thead>
+		<tr><th>Date</th><th>Référence</th><th>Avion</th><th>Pilote</th><th>Prix du vol</th><th>Durée</th></tr>
+	</thead>
+	<tbody class="table-group-divier">
+<?php
+	foreach($all_inits as $tr)
+		print($tr) ;
+?>
+	</tbody>
+</table>
+<h2>Vols découvertes (IF)</h2>
+<table class="table table-striped table-responsive col-md-6 col-xs-12">
+	<thead>
+		<tr><th>Date</th><th>Référence</th><th>Avion</th><th>Pilote</th><th>Prix du vol</th><th>Durée</th></tr>
+	</thead>
+	<tbody class="table-group-divier">
+<?php
+	foreach($all_ifs as $tr)
+		print($tr) ;
+?>
+	</tbody>
 </table>
 <?php
 require_once 'flight_trailer.php' ;
