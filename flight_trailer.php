b02abe4490b0a981d3d05fe72f1c989b095a2755
Eric Vyncke <eric@vyncke.org>
2024-05-06 20:41:14 +0200

    Add Patrick as co-developper

--- flight_trailer.php
+++ flight_trailer.php
@@ -4,7 +4,7 @@
 </div><!-- row -->
 
 <div class="row">
-	<span class="copyright">R&eacute;alisation: Eric Vyncke, avril 2018 - novembre 2023, pour RAPCS, Royal A&eacute;ro Para Club de Spa, ASBL</span><br/>
+	<span class="copyright">Réalisation: Eric Vyncke, avril 2018 - 2024, Patrick Reginter 2022-2024 pour RAPCS, Royal Aéro Para Club de Spa, ASBL</span><br/>
 	<span class="copyright">Open source code at <a href="https://github.com/evyncke/reserve_fly">github</a>.</span>
 </div><!-- row -->
 
