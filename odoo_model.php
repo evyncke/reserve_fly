ad858735a369940f07b5f6ee1e86613ac4a2ba82
Eric Vyncke <eric@vyncke.org>
2024-05-08 08:00:20 +0200

    Guard against empty result

--- odoo_model.php
+++ odoo_model.php
@@ -49,22 +49,28 @@ $odooClient = new OdooClient($odoo_host, $odoo_db, $odoo_username, $odoo_passwor
 $odooClient->debug = true ;
 
 if ($id != '') { // Display all field of this line
-    print("<thead><tr><th>Field Name</th><th>Field Value</th></tr><tbody class=table-divider>\n") ;
     $result = $odooClient->SearchRead($model, array(array(array('id','=',$id))), array()) ;
-    foreach($result[0] as $f=>$desc) {
-        $value = (isset($desc)) ? $desc : '' ;
-        if (is_array($value)) $value = '[' . implode(', ', $value) . ']';
-        print("<tr><td><a href=\"?model=$model&name=$f\">$f</a></td><td>$value</td></tr>\n") ;
-    }
+    if ($result) {
+        print("<thead><tr><th>Field Name</th><th>Field Value</th></tr><tbody class=table-divider>\n") ;
+        foreach($result[0] as $f=>$desc) {
+            $value = (isset($desc)) ? $desc : '' ;
+            if (is_array($value)) $value = '[' . implode(', ', $value) . ']';
+            print("<tr><td><a href=\"?model=$model&name=$f\">$f</a></td><td>$value</td></tr>\n") ;
+        }
+    } else
+        print("<div class=\"text-bg-danger\">Record #$id not found.</div>") ;
 
 } else if ($name != '') { // Display all lines with this field
-    print("<thead><tr><th>Id</th><th>Name</th><th>Field Name</th><th>Field Value</th></tr><tbody class=table-divider>\n") ;
     $result = $odooClient->SearchRead($model, array(), array('fields' => array('id', 'name', $name))) ;
-    foreach($result as $f=>$desc) {
-        $value = (isset($desc[$name])) ? $desc[$name] : '' ;
-        if (is_array($value)) $value = '[' . implode(', ', $value) . ']';
-        print("<tr><td><a href=\"?model=$model&id=$desc[id]\">$desc[id]</a></td><td>$desc[name]</td><td>$name</td><td>$value</td></tr>\n") ;
-    }
+    if ($result) {
+           print("<thead><tr><th>Id</th><th>Name</th><th>Field Name</th><th>Field Value</th></tr><tbody class=table-divider>\n") ;
+        foreach($result as $f=>$desc) {
+            $value = (isset($desc[$name])) ? $desc[$name] : '' ;
+            if (is_array($value)) $value = '[' . implode(', ', $value) . ']';
+            print("<tr><td><a href=\"?model=$model&id=$desc[id]\">$desc[id]</a></td><td>$desc[name]</td><td>$name</td><td>$value</td></tr>\n") ;
+        }
+    } else
+    print("<div class=\"text-bg-danger\">Name <b>$name</b> not found.</div>") ;
 
 } else {
     // Let's get all Odoo fields from the model
