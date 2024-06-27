e9d16a57e96857e0731bd1f340d563ee71fda05e
Eric Vyncke <eric@vyncke.org>
2024-06-27 07:51:16 +0200

    Add François as full weather

--- reservation.js
+++ reservation.js
@@ -334,19 +334,19 @@ function displayMETAR() {
 					elem.innerHTML =  metar_response.error ;
 				} else {
 					elem.innerHTML = '<b>' +  metar_response.METAR + '</b>' ;
-					if (userId == 62 && metar_response.temperature &&  metar_response.dew_point && (( metar_response.temperature -  metar_response.dew_point) <= 10))
+					if ((userId == 62 || userId == 102) && metar_response.temperature &&  metar_response.dew_point && (( metar_response.temperature -  metar_response.dew_point) <= 10))
 						elem.innerHTML += '<br/><i>Relative humidity: ' +
 							Math.round(100.0 - 5.0 * ( metar_response.temperature - metar_response.dew_point)) + '%</i>' ;
-					if (userId == 62 && metar_response.clouds_base) {
+					if ((userId == 62 || userId == 102) && metar_response.clouds_base) {
 						elem.innerHTML += '<br/><i>Theorical clouds base at ' +  metar_response.station + ': ' +  metar_response.clouds_base + ' ft</i>' ;
 					}
-					if (userId == 62 && metar_response.density_altitude) {
+					if ((userId == 62 || userId == 102) && metar_response.density_altitude) {
 						elem.innerHTML += '<br/><i>Density altitude at ' +  metar_response.station + ': ' +  metar_response.density_altitude +
 							' ft, elevation: ' +  metar_response.elevation + ' ft</i>';
 						if ( metar_response.density_altitude >  metar_response.elevation + 1500)
 							elem.innerHTML += '<br/><i><span style="color: red; font-size: x-large;">Caution, high density altitude!!!</span></i>' ;
 					}
-					if (userId == 62 && metar_response.pressure_altitude) {
+					if ((userId == 62 || userId == 102) && metar_response.pressure_altitude) {
 						elem.innerHTML += '<br/><i>Pressure altitude at ' +  metar_response.station + ': ' +  metar_response.pressure_altitude + ' ft</i>' ;
 					}
 					if (metar_response.condition != null &&  metar_response.condition == 'VMC')
