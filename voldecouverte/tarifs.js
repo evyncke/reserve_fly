1f86cc0224ead2fa110ea7ea4fb30e9497519658
Patrick Reginster <patrick.reginster@gmail.com>
2024-12-11 15:12:04 +0100

    Nouveaux prix vols init et if

--- voldecouverte/tarifs.js
+++ voldecouverte/tarifs.js
@@ -1,15 +1,15 @@
 var my_tarifs = [ 
-{ id: 1, tarif: 225, name: "Initiation 1 passager", type:"initiation", passenger:1},
-{ id: 2, tarif: 270, name: "Initiation 2 passagers", type:"initiation", passenger:2},
-{ id: 3, tarif: 280, name: "Initiation 3 passagers", type:"initiation", passenger:3},
-{ id: 4, tarif: 3.4, name: "Decouverte 1 passager", type:"decouverte", passenger:1},
-{ id: 5, tarif: 4.8, name: "Decouverte 2 passagers", type:"decouverte", passenger:2},
-{ id: 6, tarif: 4.8, name: "Decouverte 3 passagers", type:"decouverte", passenger:3},
-{ id: 7, tarif: 2.7, name: "Membre 1 passager", type:"membre", passenger:1},
-{ id: 8, tarif: 4.1, name: "Membre 2 passagers", type:"membre", passenger:2},
-{ id: 9, tarif: 4.1, name: "Membre 3 passagers", type:"membre", passenger:3},
-{ id: 10, tarif: 60, name: "Cotisation non navigant", type:"cotisation", passenger:1},
-{ id: 11, tarif: 2.13, name: "Pilote 1 passager", type:"pilote", passenger:1},
-{ id: 12, tarif: 3.32, name: "Pilote 2 passagers", type:"pilote", passenger:2},
-{ id: 13, tarif: 3.32, name: "Pilote 3 passagers", type:"pilote", passenger:3}
+{ id: 1, tarif: 240, name: "Initiation 1 passager", type:"initiation", passenger:1},
+{ id: 2, tarif: 285, name: "Initiation 2 passagers", type:"initiation", passenger:2},
+{ id: 3, tarif: 295, name: "Initiation 3 passagers", type:"initiation", passenger:3},
+{ id: 4, tarif: 3.7, name: "Decouverte 1 passager", type:"decouverte", passenger:1},
+{ id: 5, tarif: 5.1, name: "Decouverte 2 passagers", type:"decouverte", passenger:2},
+{ id: 6, tarif: 5.1, name: "Decouverte 3 passagers", type:"decouverte", passenger:3},
+{ id: 7, tarif: 3.0, name: "Membre 1 passager", type:"membre", passenger:1},
+{ id: 8, tarif: 4.4, name: "Membre 2 passagers", type:"membre", passenger:2},
+{ id: 9, tarif: 4.4, name: "Membre 3 passagers", type:"membre", passenger:3},
+{ id: 10, tarif: 70, name: "Cotisation non navigant", type:"cotisation", passenger:1},
+{ id: 11, tarif: 2.19, name: "Pilote 1 passager", type:"pilote", passenger:1},
+{ id: 12, tarif: 3.37, name: "Pilote 2 passagers", type:"pilote", passenger:2},
+{ id: 13, tarif: 3.37, name: "Pilote 3 passagers", type:"pilote", passenger:3}
 ]; 
