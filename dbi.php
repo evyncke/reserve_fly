e0f8da133875778392a031554c37387c26303bb7
Eric Vyncke <eric@vyncke.org>
2024-12-29 16:48:32 +0100

    Don't intercept AJAX for membership

--- dbi.php
+++ dbi.php
@@ -328,7 +328,7 @@ if ($userId > 0 and $userId != 294) { // Only for logged-in users and not for SP
 	if (!$row_fee) {
 		$cb = urlencode($_SERVER['PHP_SELF'] . '?' . $_SERVER['QUERY_STRING']) ;
 		if ($_SERVER['PHP_SELF'] != '/resa/mobile_journal.php' && $_SERVER['PHP_SELF'] != '/resa/mobile_membership.php' && 
-			$_SERVER['PHP_SELF'] != '/resa/get_bookings.php.php' && $_SERVER['PHP_SELF'] != '/resa/get_fi_agenda.php')
+			$_SERVER['PHP_SELF'] != '/resa/get_bookings.php' && $_SERVER['PHP_SELF'] != '/resa/get_fi_agenda.php')
 			if (!isset($_COOKIE['membership'])) {
 					journalise($userId, "I", "Unpaid membership, redirecting to membership page") ;
 					header("Location: https://www.spa-aviation.be/resa/mobile_membership.php?cb=" . urlencode($_SERVER['PHP_SELF'] . '?' . $_SERVER['QUERY_STRING']) , TRUE, 307) ;
