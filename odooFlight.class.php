83e70b0b1643357472c5e5f1584216d47f533b53
Eric Vyncke <eric@vyncke.org>
2025-04-28 08:40:53 +0200

    TODO use invoice_payment_term_id in Odoo

--- odooFlight.class.php
+++ odooFlight.class.php
@@ -1416,6 +1416,7 @@ function OF_InvoiceDueDays($theOdooPartnerReference)
 //============================================
 function OF_LastInvoiceDueDate($theOdooPartnerReference)
 {
+    // TODO added by evyncke 2025-04-28: new Odoo seems to use invoice_payment_term_id rather than invoice_date_due
     //print("OF_LastInvoiceDueDate($theOdooPartnerReference)<br>");
     $odooClient=OF_GetOdooClient();
     $result= $odooClient->SearchRead('account.move', 
