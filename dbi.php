52216717ba6aa33351097b3571caf646228e3caa
Eric Vyncke <eric@vyncke.org>
2025-05-21 17:28:42 +0200

    Remove old webcam

--- dbi.php
+++ dbi.php
@@ -60,7 +60,7 @@ $nav_membership_price = 200.0 ; //2025
 $membership_analytic_account = 25  ;
 
 $tracked_planes = array('OOALD', 'OOALE', 'OOAPV', 'OOFUN', 'OOJRB', 'OOFMX', 'OOSPQ') ;
-array_push($tracked_planes, 'OOADO', 'OOG85', 'OOPEG', 'OOVMS', 'DELZA', 'DELZB', 'FJXRL') ; // For Air Spa Rallye
+// array_push($tracked_planes, 'OOADO', 'OOG85', 'OOPEG', 'OOVMS', 'DELZA', 'DELZB', 'FJXRL') ; // For Air Spa Rallye
 // array_push($tracked_planes, 'OOCEK', 'FAZMX', 'FAYAC', 'GIIIG') ; // For 75 ans avions externes
 
 $bccTo = "eric.vyncke@edpnet.be" ;
@@ -211,9 +211,10 @@ $validity_warning = 30 ; // Number of days before validity expiration to flag as
 //$webcam_uris = array("https://www.skydivespa.be/webcam/cam2/image.jpg", "https://www.spa-aviation.be/webcam.php") ;
 // $webcam_uris = array("https://www.spa-aviation.be/webcam.php", "https://s24.ipcamlive.com/streams/18rerbje6iohwnfjw/snapshot.jpg") ; 2025-01-04
 // $webcam_uris = array("https://www.spa-aviation.be/webcam.php", "https://g0.ipcamlive.com/player/player.php?alias=camebspairside&autoplay=1") ;
-$webcam_uris = array("https://nav.vyncke.org/snapshot.jpg",
+$webcam_uris = array("https://nav.vyncke.org/rapcs/snapshot.jpg",
 	"https://ipcamlive.com/player/snapshot.php?alias=camebspairside", 
-	"https://www.spa-aviation.be/webcam.php",) ;
+//	"https://www.spa-aviation.be/webcam.php",
+	) ;
 $favicon = "https://www.spa-aviation.be/favicon32x32.ico" ;// The usual web browser favicon + also used in Calendar
 $ical_name = "Calendrier réservations et événements RAPCS" ; // Name of the iCAL calendar
 $ical_organizer = "RAPCS asbl" ; // Name of the organizer of the iCAL calendar
