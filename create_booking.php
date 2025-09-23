9e25fa46aa252c0fcf35dcdb0d756ada4cdfc471
Eric Vyncke <eric@vyncke.org>
2025-09-23 14:58:20 +0200

    Log referer script name

--- create_booking.php
+++ create_booking.php
@@ -24,6 +24,13 @@ $mime_preferences = array(
 	"output-charset" => "UTF-8",
 	"scheme" => "Q") ;
 
+$referer = isset($_SERVER['HTTP_REFERER']) ? $_SERVER['HTTP_REFERER'] : '';
+if ($referer != '') {
+	$path = parse_url($referer, PHP_URL_PATH);
+	$script_name = basename($path);   
+} else {
+	$script_name = '';
+}
 $response = array() ;
 $response['error'] = '' ; // Until now: no error :-)
 
@@ -440,7 +447,7 @@ if ($response['error'] == '') {
 		if ($booking_type == BOOKING_MAINTENANCE)
 			journalise($userId, 'W', "$plane is out for maintenance #$booking_id by $booker[name] ($comment). $start => $end") ;
 		else {
-			journalise($userId, 'I', "Booking #$booking_id of $plane done for $pilot[name] by $booker[name] ($comment). $start => $end") ;
+			journalise($userId, 'I', "Booking #$booking_id of $plane done by $script_name for $pilot[name] by $booker[name] ($comment). $start => $end") ;
 			// Check for long booking...
 			date_default_timezone_set('Europe/Brussels') ;
 			$interval = date_diff(date_create($end), date_create($start)) ;
