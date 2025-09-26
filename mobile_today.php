553434ffa61ed86ad1aa789e497d4c1b5212e5b0
Eric Vyncke <eric@vyncke.org>
2025-09-26 18:24:56 +0200

    Use date rather than time query parameter + add spinner

--- mobile_today.php
+++ mobile_today.php
@@ -27,7 +27,19 @@ $header_postamble = '
 ' ;
 require_once 'mobile_header5.php' ;
 
-$displayTimestamp = (isset($_REQUEST['time'])) ? intval($_REQUEST['time']) : time() ;
+$displayDate = isset($_REQUEST['date']) ? mysqli_real_escape_string($mysqli_link, $_REQUEST['date']) : date('Y-m-d') ;
+$dt = new DateTime($displayDate);
+
+// Day before
+$dt_before = clone $dt;
+$dt_before->modify('-1 day');
+$day_before = $dt_before->format('Y-m-d');
+
+// Day after
+$dt_after = clone $dt;
+$dt_after->modify('+1 day');
+$day_after = $dt_after->format('Y-m-d');
+
 // Display today in the local language in human language
 $fmt = datefmt_create(
     'fr_BE',
@@ -37,8 +49,19 @@ $fmt = datefmt_create(
     IntlDateFormatter::GREGORIAN,
     'EEEE d MMMM yyyy' // See https://unicode-org.github.io/icu/userguide/format_parse/datetime/ !
 ) ;
-$today_nice = datefmt_format($fmt, $displayTimestamp) ; // Nicely locally formatted date
-$sql_date = date('Y-m-d', $displayTimestamp) ;
+
+$short_fmt = datefmt_create(
+    'fr_BE',
+    IntlDateFormatter::FULL,
+    IntlDateFormatter::FULL,
+    'Europe/Brussels',
+    IntlDateFormatter::GREGORIAN,
+    'EEEE d MMMM' // See https://unicode-org.github.io/icu/userguide/format_parse/datetime/ !
+) ;
+
+$today_nice = datefmt_format($fmt, $dt) ; // Nicely locally formatted date
+$day_before_nice = datefmt_format($short_fmt, $dt_before) ; // Nicely locally formatted date
+$day_after_nice = datefmt_format($short_fmt, $dt_after) ; // Nicely locally formatted date
 $sql_today = date('Y-m-d') ;
 $sql_now = date('Y-m-d H:i:s') ;
 if ($userId != 62) journalise($userId, "D", "Using smartphone booking page for $today_nice") ;
@@ -48,9 +71,9 @@ if ($userId != 62) journalise($userId, "D", "Using smartphone booking page for $
 <div class="page-header">
 <h2>&#128241; Réservations des avions</h2>
 <ul class="pagination justify-content-center">
-	<li class="page-item"><a class="page-link" href="<?=$_SERVER['PHP_SELF'] . '?time=' . ($displayTimestamp - 24 * 3600)?>">Jour précédent</a></li>
+	<li class="page-item"><a class="page-link" href="<?=$_SERVER['PHP_SELF'] . '?date=' . $day_before?>"><?=$day_before_nice?></a></li>
 	<li class="page-item"><a class="page-link active" href="#"><?=$today_nice?></a></li>
-	<li class="page-item"><a class="page-link" href="<?=$_SERVER['PHP_SELF'] . '?time=' . ($displayTimestamp + 24 * 3600)?>">Jour suivant</a></li>
+	<li class="page-item"><a class="page-link" href="<?=$_SERVER['PHP_SELF'] . '?date=' . $day_after?>"><?=$day_after_nice?></a></li>
 </ul> <!-- pagination -->
 </div> <!-- page-header -->
 
@@ -70,7 +93,7 @@ if ($userId != 62) journalise($userId, "D", "Using smartphone booking page for $
 		LEFT JOIN $table_person i ON i.jom_id = r_instructor		
 		JOIN $table_planes p ON r_plane = p.id
 		LEFT JOIN $table_flights fl ON r_id = f_booking
-		WHERE  p.actif = 1 AND p.ressource = 0 AND r_cancel_date IS NULL AND (DATE(r_stop) = '$sql_date' OR (DATE(r_start) <= '$sql_date' and '$sql_date' <= DATE(r_stop)))
+		WHERE  p.actif = 1 AND p.ressource = 0 AND r_cancel_date IS NULL AND (DATE(r_stop) = '$displayDate' OR (DATE(r_start) <= '$displayDate' and '$displayDate' <= DATE(r_stop)))
 		ORDER BY r_start, r_plane ASC LIMIT 0,20")
 		or die("Cannot retrieve bookings($plane): " . mysqli_error($mysqli_link)) ;
 	$rows = array() ;
@@ -115,7 +138,7 @@ if ($userId != 62) journalise($userId, "D", "Using smartphone booking page for $
 			db2web($row['ifirst_name']) . ' ' . db2web($row['ilast_name']) . '">' .
 			substr($row['ifirst_name'], 0, 1) . "." . substr($row['ilast_name'], 0, 1) . '. </span></i>' . $itelephone : '' ; 
 		// Add an orange divider representing 'now'
-		if ($sql_today == $sql_date and !$now_divider_shown and $row['r_start'] >= $sql_now) {
+		if ($sql_today == $displayDate and !$now_divider_shown and $row['r_start'] >= $sql_now) {
 			$now_divider_shown = true;
 			print('</tbody><tbody class="table-group-divider" style="border-top: 4px solid #ffc107; "><!--tr><td colspan="5" class="text-bg-warning"></td></tr-->') ;
 		}
@@ -123,20 +146,30 @@ if ($userId != 62) journalise($userId, "D", "Using smartphone booking page for $
 		if ($row['f_type'] != '')
 			$class = ' class="text-warning"' ;
 		// Display date if not today, else display time
-		if (strpos($row['r_start'], $sql_date) === 0) 
+		if (strpos($row['r_start'], $displayDate) === 0) 
 			$display_start = substr($row['r_start'], 11) ;
 		else
 			$display_start = substr($row['r_start'], 0, 10) ;
-		if (strpos($row['r_stop'], $sql_date) === 0) 
+		if (strpos($row['r_stop'], $displayDate) === 0) 
 			$display_stop = substr($row['r_stop'], 11) ;
 		else
 			$display_stop = substr($row['r_stop'], 0, 10) ;
+		// If the booking is for maintenance, show it in red and don't display the booker's name
+		if ($row['r_type'] == BOOKING_MAINTENANCE) {
+			$class = ' class="text-danger"' ;
+			$pname = '<i class="bi bi-tools"></i> <i>Maintenance</i>' ;
+			$ptelephone = '' ;
+			$instructor = '' ;
+		}
+		// Make the row clickable to show the details in a modal dialog
 		$onclick = " onclick=\"showDetails($row[r_id]);\" data-bs-target=\"#detailModal\"" ;
 		print("<tr$onclick><td$class>$row[r_plane]</td><td$class>$display_start</td><td$class>$display_stop</td><td$class>$pname$ptelephone$instructor</td><td$class>". nl2br(db2web($row['r_comment'])) . "</td></tr>\n") ;
 	}
 	// Add an orange divider representing 'now' if not yet done
-	if ($sql_today == $sql_date and !$now_divider_shown and $result->num_rows > 0)
+	if ($sql_today == $displayDate and !$now_divider_shown and $result->num_rows > 0) {
 		print('</tbody><tbody class="table-group-divider" style="border-top: 4px solid #ffc107; "><tr><td colspan="5">Plus de réservations pour aujourd\'hui</td></tr>') ;
+		$now_divider_shown = true;
+	}
 	if ($result->num_rows == 0)
 		print('<tr><td colspan="5" class="text-bg-info">Aucune réservation pour ce jour</td></tr>') ;
 ?>
@@ -155,7 +188,7 @@ if ($userId != 62) journalise($userId, "D", "Using smartphone booking page for $
               	<div class="modal-body" id="detailModalContent">
 					<div id="modalSpinner" class="d-none"
 			           style="position:absolute; top:0; left:0; width:100%; height:100%; background:rgba(255,255,255,0.7); z-index:1051; display:flex; align-items:center; justify-content:center;">
-        				<div class="spinner-border" style="width:4rem; height:4rem;" role="status"></div>
+        				<div class="spinner-border" style="width:4rem; height:4rem;" role="status"><span class="visually-hidden">En cours...</span></div>
 					</div>
 					<img id="pilotDetailsImage"><span id="pilotDetailsSpan"></span>
 					Avion: <select id="planeSelect"></select>
@@ -184,7 +217,7 @@ if ($userId != 62) journalise($userId, "D", "Using smartphone booking page for $
     </div>
 </div>
 
-<button type="button" class="btn btn-primary" onclick="window.location.href='mobile_book.php'">
+<button type="button" class="btn btn-primary" onclick="window.location.href='mobile_book.php?date=<?=$displayDate?>';">
   <i class="bi bi-plus"></i> Nouvelle réservation
 </button>
 
