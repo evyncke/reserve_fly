0a950fcfc8af5f582df6482b380ad3926746306b
Eric Vyncke <eric@vyncke.org>
2026-08-25 14:30:04 +0200

    Ensure to escape journalise inputs

--- mobile_help.php
+++ mobile_help.php
@@ -24,19 +24,21 @@ if (isset($_REQUEST['topic']) and $_REQUEST['topic'] != '') {
     if (!preg_match('/^[a-zA-Z0-9._-]+$/', $topic) or // only safe chars
             strpos($topic, '..') !== false or            // no directory traversal
             strpos($topic, '/') !== false)                // no slashes
-        journalise($userId, "F", "Invalid help topic: $topic") ;
+        journalise($userId, "F", "Invalid help topic: " . htmlspecialchars($topic, ENT_QUOTES | ENT_SUBSTITUTE, 'UTF-8')) ;
 } else 
     $topic = 'main' ;
 
-if ($userId != 62) journalise($userId, "I", "Help topic: $topic, referer: $referer_name") ;
-// TODO use referer to get to specific help page if the relevant help/*.ms exists and no topic is specified
+if ($userId != 62) journalise($userId, "I", "Help topic: " . htmlspecialchars($topic, ENT_QUOTES | ENT_SUBSTITUTE, 'UTF-8') . 
+    ", referer: " . htmlspecialchars($referer_name, ENT_QUOTES | ENT_SUBSTITUTE, 'UTF-8')) ;
+// TODO use referer to get to specific help page if the relevant help/*.md exists and no topic is specified
 ?>
 <div class="container">
 <div class="row">
 <?php
 $Parsedown = new Parsedown();
+// $Parsedown->setSafeMode(true);
 $text = file_get_contents("help/$topic.md") 
-    or journalise($userId, "F", "Cannot read help file help/$topic.md") ;
+    or journalise($userId, "F", "Cannot read help file help/" . htmlspecialchars($topic, ENT_QUOTES | ENT_SUBSTITUTE, 'UTF-8') . ".md") ;
 echo $Parsedown->text($text) ;
 ?>
 </div><!-- row -->
