83c88c917a00cff2d50105a5520ac8755346ecf0
Eric Vyncke <eric@vyncke.org>
2026-09-03 18:53:35 +0200

    Define siteURL in JS

--- IntroCarnetVol.php
+++ IntroCarnetVol.php
@@ -23,9 +23,9 @@ require_once 'dto.class.php';
 require_once 'IntroCarnetVol_tools.php' ;
 
 # HTTP/2 push of JS & CSS via header() to go faster
-header('Link: </resa/js/script_carnetdevol.js>;rel=preload;as=script,</resa/data/members.js>;rel=preload;as=script,</resa/data/pilots.js>;rel=preload;as=script,' .
-	'</resa/data/instructors.js>;rel=preload;as=script,' .
-	'</resa/data/planes.js>;rel=preload;as=script,</resa/data/shareCodes.js>;rel=preload;as=script,</resa/data/prix.js>;rel=preload;as=script,</resa/css/IntroCarnetVol.css>;rel=preload;as=style') ;
+header('Link: </' . SITE_PATH. '/js/script_carnetdevol.js>;rel=preload;as=script,</' . SITE_PATH . '/data/members.js>;rel=preload;as=script,</' . SITE_PATH . '/data/pilots.js>;rel=preload;as=script,' .
+	'</' . SITE_PATH . '/data/instructors.js>;rel=preload;as=script,' .
+	'</' . SITE_PATH . '/data/planes.js>;rel=preload;as=script,</' . SITE_PATH . '/data/shareCodes.js>;rel=preload;as=script,</' . SITE_PATH . '/data/prix.js>;rel=preload;as=script,</' . SITE_PATH . '/css/IntroCarnetVol.css>;rel=preload;as=style') ;
 
 if ($userIsAdmin or $userIsInstructor) { // Let' trust this browser for one year 
 	// TODO only send it when not received
@@ -36,11 +36,12 @@ if ($userIsAdmin or $userIsInstructor) { // Let' trust this browser for one year
 <html lang="fr">
 <head>
   <title>Introduction carnet de routes</title>
-  <link rel="stylesheet" type="text/css" href="css/IntroCarnetVol.css">
+  <link rel="stylesheet" type="text/css" href="<?= SITE_URL ?>css/IntroCarnetVol.css">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link href="https://<?= SITE_HOST ?>/favicon32x32.ico" rel="shortcut icon" type="image/vnd.microsoft.icon">
 <script>
-	
+var siteURL = "<?= SITE_URL ?>";
+
 <?php
 //---------------------------------------------------------------------------
 // Initialiation of default variables
@@ -1376,7 +1377,7 @@ Communication : "<span id="id_payment_communication"></span>"</br>Compte : BE64 
 <p><center><input class="button" type="button" style="background-color: green; color: white;" value="Mon Folio" onclick="javascript:document.location.href='<?= SITE_URL ?>mobile_folio.php';"></input></center>
 <center><input class="button" type="button" style="background-color: green; color: white;" value="Accès au TechLog" onclick="javascript:document.location.href='<?= SITE_URL ?>mobile_incidents.php';"></input></center>
 <center><input class="button" type="button" style="background-color: green; color: white;" value="Mon carnet de vol" onclick="javascript:document.location.href='<?= SITE_URL ?>mobile_mylog.php';"></input></center>
-<center><input class="button" type="button" style="background-color: green; color: white;" value="Retour à la page de réservation" onclick="javascript:document.location.href='https://<?= SITE_HOST ?>/index.php/fr/resa-full/reserver';"></input></center></p>
+<center><input class="button" type="button" style="background-color: green; color: white;" value="Retour à la page de réservation" onclick="javascript:document.location.href='<?= SITE_URL ?>mobile_reservation.php';"></input></center></p>
 <p></p>
 <p></p>
 <p><b>Syntaxe:</b></p>
@@ -1384,11 +1385,11 @@ Communication : "<span id="id_payment_communication"></span>"</br>Compte : BE64 
 <p>Durée: "2:20", "2h20", "140"</p>
 <p>Compteur : xxxxyy (OO-APV xxxxy)  xxxx.yy (OO-APV xxxx.y) ou xxxx,yy  (OO-APV xxxx,y)</p>
 <!-- Those scripts must be at the end and not in the header as some prefil operations are not done in the onLoad() :-( ...-->
-<script src="data/planes.js" ></script>
-<script src="data/instructors.js"></script>
-<script src="data/members.js"></script>
-<script src="data/shareCodes.js"></script>
-<script src="data/pilots.js"></script>
+<script src="<?= SITE_URL ?>data/planes.js" ></script>
+<script src="<?= SITE_URL ?>data/instructors.js"></script>
+<script src="<?= SITE_URL ?>data/members.js"></script>
+<script src="<?= SITE_URL ?>data/shareCodes.js"></script>
+<script src="<?= SITE_URL ?>data/pilots.js"></script>
 <script src="<?= SITE_URL ?>data/prix.js"></script>
 <script src="<?= SITE_URL ?>js/script_carnetdevol.js"></script>
 </body>
